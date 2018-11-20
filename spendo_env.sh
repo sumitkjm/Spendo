94a3585a838dea35fd0456299d5c3bbd2b455c6e
Sumit Kumar <sumitjangir@gmail.com>
2018-11-20 16:29:18 +0530

    Adding Spending Environment

---
+++ spendo_env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home
+export M2_HOME=/Applications/Softwares/apache-maven-3.3.9
+export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
+alias spendo_home='cd /Users/skumar6/git/Spendo'
+alias mvn='mvn -s /Users/skumar6/.maven_repo/settings.xml'
